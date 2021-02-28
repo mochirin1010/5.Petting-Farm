66d7034854ca3fe6a7a4258a802ae43642ed9d4d
yamajimomoyo <mochirin0530@gmail.com>
2021-02-28 23:26:46 +0900

    update file

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -9,7 +9,6 @@ document.addEventListener("turbolinks:load", function() {$(function() {
   });
 })
 
-
 document.addEventListener("turbolinks:load", function() {$(function() {
   function readURL(input) {
       if (input.files && input.files[0]) {
