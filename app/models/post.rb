af8579c505a90654698a94fd97dfc3a02312b634
yamajimomoyo <mochirin0530@gmail.com>
2021-03-16 17:14:07 +0900

    update-file

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   validates :image, { presence: true }
   validates :user_id, { presence: true }
   belongs_to :user
+  belongs_to :pet
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user, dependent: :destroy
   has_ancestry
