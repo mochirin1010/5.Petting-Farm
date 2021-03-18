cc7474445044493a83800d94125c72d469409c56
yamajimomoyo <mochirin0530@gmail.com>
2021-03-18 16:07:33 +0900

    edit-redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(_resource)
-    posts_path
-  end
-
-  def after_sign_out_path_for(_resource)
-    new_user_session_path
-  end
-
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
-
   protected
 
   def configure_permitted_parameters
